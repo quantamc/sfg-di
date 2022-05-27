3c40815d75c1953b439b18ec18d6c6d94149ee62
thabomalete <thabo_malete@outlook.com>
2022-05-27 17:36:54 +0200

    Config services

--- src/main/java/guru/springfranework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springfranework/sfgdi/services/I18nSpanishGreetingService.java
@@ -1,10 +1,6 @@
 package guru.springfranework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
-@Profile({"ES", "default"})
-@Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
 
     @Override
